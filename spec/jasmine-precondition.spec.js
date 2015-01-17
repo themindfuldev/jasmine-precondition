0b17d5e7d17623d447cb2e7861211bd93749fec6
Tiago Garcia <tiagoromerogarcia@gmail.com>
2015-01-17 12:21:54 -0800

    improving tests and performance

--- spec/jasmine-precondition.spec.js
+++ spec/jasmine-precondition.spec.js
@@ -2,21 +2,39 @@
 
 var preCondition = require('../lib/jasmine-precondition');
 
-describe('the preCondition lib', function () {
+describe('the preCondition command', function () {
 
   describe('working with counters', function() {
 
-    var counter = 0;
+    var counter1 = 0,
+        counter2 = 0,
+        interval;
 
     beforeEach(function(done) {
+      interval = setInterval(function(){
+        counter1 += 100;
+      }, 100);
+
       preCondition(function() {
-        counter += 100;
-        return counter === 500;
+        return counter1 >= 500;
       }, done, 100);
     });
 
-    it('should get executed only when counter is 500', function () {
-      expect(counter).toBe(500);
+    it('should only get executed when counter1 is 500', function (done) {
+      expect(counter1).toBe(500);
+
+      preCondition(function() {
+        counter2 += 200;
+        return counter2 === 1000;
+      }, done, 100);
+    });
+
+    it('should only get executed when counter2 is 1000', function () {
+      expect(counter2).toBe(1000);
+    });
+
+    afterEach(function(){
+      clearInterval(interval);
     });
 
   });
