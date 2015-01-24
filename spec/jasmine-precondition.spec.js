3adb9695dbf556e2288aa1e63b4af295baac4e07
Tiago Garcia <tiagoromerogarcia@gmail.com>
2015-01-23 18:08:26 -0800

    adding test to browser loading

--- spec/jasmine-precondition.spec.js
+++ spec/jasmine-precondition.spec.js
@@ -1,17 +1,30 @@
 'use strict';
 
 var preCondition = require('../lib/jasmine-precondition');
+var fs = require('fs');
+var path = require('path');
 
 describe('the preCondition instruction', function () {
 
-  it('should be loaded', function(){
-    expect(typeof preCondition === 'function').toBeTruthy();
-  });
+  describe('initialization', function () {
 
-  describe('initialization', function() {
+    it('should be loaded through require', function () {
+      expect(typeof preCondition === 'function').toBeTruthy();
+    });
+
+    it('should be loaded through the browser', function () {
+      var filePath = path.join(__dirname, '../lib/jasmine-precondition.js');
+      var file = fs.readFileSync(filePath, { encoding: 'utf-8' });
+      var runner = new Function(file);
+
+      var window = {}
+      runner.apply(window);
+
+      expect(typeof window.preCondition === 'function').toBeTruthy();
+    });
 
     it('should get executed when no interval is passed', function (done) {
-      preCondition(function(){
+      preCondition(function (){
         return true;
       }, done);
     });
@@ -37,18 +50,18 @@ describe('the preCondition instruction', function () {
     });
   });
 
-  describe('working with counters', function() {
+  describe('working with counters', function () {
 
     var counter1 = 0,
         counter2 = 0,
         interval;
 
-    beforeEach(function(done) {
-      interval = setInterval(function(){
+    beforeEach(function (done) {
+      interval = setInterval(function (){
         counter1 += 100;
       }, 100);
 
-      preCondition(function() {
+      preCondition(function () {
         return counter1 >= 500;
       }, done, 100);
     });
@@ -56,7 +69,7 @@ describe('the preCondition instruction', function () {
     it('should only get executed when counter1 is 500', function (done) {
       expect(counter1).toBe(500);
 
-      preCondition(function() {
+      preCondition(function () {
         counter2 += 200;
         return counter2 === 1000;
       }, done, 100);
@@ -66,7 +79,7 @@ describe('the preCondition instruction', function () {
       expect(counter2).toBe(1000);
     });
 
-    afterEach(function(){
+    afterEach(function () {
       clearInterval(interval);
     });
 
